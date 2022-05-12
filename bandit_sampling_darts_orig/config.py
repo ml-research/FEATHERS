c185e8bbe0f2b3c6ecb7e467eb0b25297838ddff
Jonas Seng <jonas.seng@tu-darmstadt.de>
2022-05-12 09:06:47 +0200

    Fix loading error for CIFAR with label skew

--- bandit_sampling_darts_orig/config.py
+++ bandit_sampling_darts_orig/config.py
@@ -7,22 +7,22 @@ HYPERPARAM_CONFIG_NR = 120 # size of hyperparameter search space
 LOG_DIR = './runs/'
 
 # server parameters
-DATASET = 'fmnist' # dataset to use. Alternatives: cifar10
-CLIENT_NR = 2
-MIN_TRAIN_CLIENTS = 2 # min. number of clients used during fit
-MIN_VAL_CLIENTS = 2 # min. number of clients used during evaluation
+DATASET = 'cifar10' # dataset to use. Alternatives: cifar10
+CLIENT_NR = 5
+MIN_TRAIN_CLIENTS = 5 # min. number of clients used during fit
+MIN_VAL_CLIENTS = 5 # min. number of clients used during evaluation
 REINIT = False # reinitailize model if no improvement was made
 
 # model initilization parameters
 CLASSES = 10 # number of output-classes
 CELL_NR = 8 # number of cells the search space consists of (if search phase). Else number of cells of the network
-IN_CHANNELS = 1 # mumber of input-channels (e.g. 3 for rgb-images)
+IN_CHANNELS = 3 # mumber of input-channels (e.g. 3 for rgb-images)
 OUT_CHANNELS = 16 # number of output-channels
 NODE_NR = 7 # number of nodes per cell
 
-PORT = '8080'
-GPUS = [10, 11] # GPUs to use
-SERVER_GPU = 10
+PORT = '8087'
+GPUS = [4, 5, 6] # GPUs to use
+SERVER_GPU = 6
 
 DATA_SKEW = 0.5 # skew of labels. 0 = no skew, 1 only some clients hold some labels
 
