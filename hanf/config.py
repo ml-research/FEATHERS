83e751ed0cf4095166700ab40de66ec0cf1bc8ab
Jonas Seng <jonas.seng@tu-darmstadt.de>
2022-05-30 11:12:49 +0200

    Remove unused files

--- hanf/config.py
+++ hanf/config.py
@@ -1,14 +1,14 @@
 # hyperparameter configuration parameters
-ROUNDS = 1500 # nr. of communication rounds
+ROUNDS = 120 # nr. of communication rounds
 ALPHA = 0.3
 HYPERPARAM_CONFIG_NR = 120 # size of hyperparameter search space
-BATCH_SIZE = 96
+BATCH_SIZE = 64
 
 # logging
 LOG_DIR = './runs/'
 
 # server parameters
-DATASET = 'cifar10' # dataset to use. Alternatives: cifar10
+DATASET = 'fmnist' # dataset to use. Alternatives: cifar10
 CLIENT_NR = 2
 MIN_TRAIN_CLIENTS = 2 # min. number of clients used during fit
 MIN_VAL_CLIENTS = 2 # min. number of clients used during evaluation
@@ -16,9 +16,9 @@ REINIT = False # reinitailize model if no improvement was made
 
 # model initilization parameters
 CLASSES = 10 # number of output-classes
-CELL_NR = 20 # number of cells the search space consists of (if search phase). Else number of cells of the network
-IN_CHANNELS = 3 # mumber of input-channels (e.g. 3 for rgb-images)
-OUT_CHANNELS = 36 # number of output-channels
+CELL_NR = 8 # number of cells the search space consists of (if search phase). Else number of cells of the network
+IN_CHANNELS = 1 # mumber of input-channels (e.g. 3 for rgb-images)
+OUT_CHANNELS = 16 # number of output-channels
 NODE_NR = 7 # number of nodes per cell
 
 PORT = '8042'
